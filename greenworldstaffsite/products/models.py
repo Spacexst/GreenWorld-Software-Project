d0390e78c06d7949e302a43f1e6ffe77eb232df2
Yasar Guzel <sacredrider0203@gmail.com>
2024-08-17 13:42:37 +1200

    Model created

--- greenworldstaffsite/products/models.py
+++ greenworldstaffsite/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
