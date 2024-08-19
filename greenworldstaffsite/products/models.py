7f8d8f7fe1c8ad8254b75534391f7a2bf23ab239
Yasar Guzel <sacredrider0203@gmail.com>
2024-08-19 13:22:45 +1200

    Models + Title change

--- greenworldstaffsite/products/models.py
+++ greenworldstaffsite/products/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Product(models.Model):
 
-    text = models.TextField()
+    name = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return f'{self.name}: {self.description}. Price: ${self.price} '
+
+class User(models.Model):
+
+    fullname = models.CharField(255)
+    email = models.CharField(len(fullname)+15)
+    department = models.CharField(255, choices=[('Management', 'Management'), ('Marketing', 'Marketing'), ('Customer Service', 'Customer Service')])
