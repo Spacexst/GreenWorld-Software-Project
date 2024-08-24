988ccde0b23f5a2c17e2df718f64f92eb47dacc5
Spacexst <anuradha012@gmail.com>
2024-08-24 22:23:29 +1200

    Created products page

--- greenworldstaffsite/products/views.py
+++ greenworldstaffsite/products/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-
+from django.views.generic import ListView
 # Create your views here.
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = "products/product_list.html"
