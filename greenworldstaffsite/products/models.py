e729275d424ddcf17bd2ea046e665164ce53b717
Yasar Guzel <sacredrider0203@gmail.com>
2024-08-25 13:19:56 +1200

    Products page fix

--- greenworldstaffsite/products/models.py
+++ greenworldstaffsite/products/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
 
-    name = models.TextField()
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.TextField()
+    price = models.CharField(max_length=255)
 
     def __str__(self):
         return f'{self.name}: {self.description}. Price: ${self.price} '
