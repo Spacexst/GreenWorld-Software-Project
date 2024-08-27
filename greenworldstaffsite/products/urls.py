4ef7f62942dd9ab640998675fec74b43333c7de8
Spacexst <anuradha012@gmail.com>
2024-08-27 19:31:31 +1200

    added images

--- greenworldstaffsite/products/urls.py
+++ greenworldstaffsite/products/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import ProductList
+
 urlpatterns = [
     path("products/", ProductList.as_view(), name="product_list"),
+    
 ]
+
--- greenworldstaffsite/products/views.py
+++ greenworldstaffsite/products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Product
-
 from django.views.generic import ListView
+
+from .models import Product, User
+
 # Create your views here.
 
 
@@ -9,3 +10,4 @@ class ProductList(ListView):
     model = Product
     template_name = "products/product_list.html"
     context_object_name = 'product_list'
+
