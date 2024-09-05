54da55da1487cd742126045b0c52bdedd8ee1a63
Yasar Guzel <sacredrider0203@gmail.com>
2024-09-05 13:44:59 +1200

    Add/edit from admin page change staff

--- greenworldstaffsite/accounts/models.py
+++ greenworldstaffsite/accounts/models.py
@@ -5,6 +5,7 @@ class Staff(models.Model):
 
     full_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
+    department = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.full_name
